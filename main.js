9fe94f55119132df705a1a15c49b3ab6eeb854dc
jlibdev <jlibandodev@gmail.com>
2024-11-15 02:55:57 +0800

    ALLAY Body + HEAD

--- main.js
+++ main.js
@@ -191,8 +191,6 @@ import { OrbitControls } from 'three/examples/jsm/Addons.js';
 
 // tick()
 
-
-// Minecraft Basic Steve
 document.body.style.overflow = 'hidden';
 
 const canvas = document.querySelector('canvas.webgl') 
@@ -210,33 +208,36 @@ renderer.setSize( sizes.width, sizes.height )
 
 
 const camera = new THREE.PerspectiveCamera( 45,  sizes.width/sizes.height )
-camera.position.z = 3
+camera.position.z = 5
 
 const scene = new THREE.Scene();
 scene.add( camera )
 
+// ALLAY MINECRAFT
+const allay = new THREE.Group()
+
 
 // Head Group Init
 const headCube = new THREE.Group()
-scene.add(headCube)
+allay.add(headCube)
 
 
 // head Cube
 const head = new THREE.Mesh(
     new THREE.BoxGeometry(1,1,1),
-    new THREE.MeshBasicMaterial({color: 0x808080})
+    new THREE.MeshBasicMaterial({color: 0x36cbf2})
 )
 headCube.add(head)
 
 
 // Eyes
 const lefteyeCube = new THREE.Mesh(
-    new THREE.BoxGeometry(1,1,.5),
+    new THREE.BoxGeometry(1,1.5,.5),
     new THREE.MeshBasicMaterial({color: 0xffffff})
 )
 
 const righteyeCube = new THREE.Mesh(
-    new THREE.BoxGeometry(1,1,.5),
+    new THREE.BoxGeometry(1,1.5,.5),
     new THREE.MeshBasicMaterial({color: 0xffffff})
 )
 
@@ -251,6 +252,7 @@ righteyeCube.position.x = .2
 headCube.add( lefteyeCube )
 headCube.add( righteyeCube )
 
+
 // Ears
 
 const leftEar = new THREE.Mesh(
@@ -261,7 +263,7 @@ const leftEar = new THREE.Mesh(
 leftEar.scale.set(.1,.3,.3)
 leftEar.position.set(.6, .2, -.25)
 leftEar.rotation.x = Math.PI * .75
-leftEar.rotation.z = Math.PI * .10
+leftEar.rotation.z = Math.PI * .1
 headCube.add(leftEar)
 
 const rightEar = new THREE.Mesh(
@@ -269,25 +271,99 @@ const rightEar = new THREE.Mesh(
     new THREE.MeshBasicMaterial({color: 0xffffff})
 )
 
+
 rightEar.scale.set(.1,.3,.3)
 rightEar.position.set(-.6, .2, -.25)
 rightEar.rotation.x = Math.PI * .75
-rightEar.rotation.z = Math.PI * -.10
+rightEar.rotation.z = Math.PI * -.1
 headCube.add(rightEar)
+
+
 
 // const AxesHelper = new THREE.AxesHelper(1)
 // scene.add( AxesHelper )
 
+// Torso
+
+const bodyGroup = new THREE.Group()
+
+const body = new THREE.Mesh(
+    new THREE.BoxGeometry(.5,1,.5),
+    new THREE.MeshBasicMaterial({color: 0xffffff})
+)
+
+bodyGroup.add(body)
+
+body.position.y = -.5
+body.rotation.x = Math.PI * -.9
+allay.add(bodyGroup)
+
+// Hands
+
+const hands = new THREE.Group()
+
+const rightHand = new THREE.Mesh(
+    new THREE.BoxGeometry(.2,.5,.3),
+    new THREE.MeshBasicMaterial({color: 0xffffff})
+)
+
+const leftHand = new THREE.Mesh(
+    new THREE.BoxGeometry(.2,.5,.3),
+    new THREE.MeshBasicMaterial({color: 0xffffff})
+)
+
+hands.add(rightHand)
+hands.add(leftHand)
+
+allay.add(hands)
+
+rightHand.position.x = .4
+rightHand.position.y = -.8
+rightHand.rotation.z = Math.PI * -.9
+
+leftHand.position.x = -.4
+leftHand.position.y = -.8
+leftHand.rotation.z = Math.PI * .9
+
+// Controls and Camera
+
 const controls = new OrbitControls(camera, canvas)
+
 camera.position.x = 0
 
+
+// Animations
+
+// Head Animations
+
+// Eye Animation
+gsap.to([righteyeCube.scale,lefteyeCube.scale], {y: 0.02, ease: 'power2.inOut', yoyo: true, repeat: -1 , repeatDelay: 5})
+gsap.to(headCube.rotation, {z: Math.PI * -.05, x:  Math.PI * -.05,  ease: 'power2.inOut', yoyo: true, repeat: -1 , repeatDelay: 5})
+
+// Ear Animation
+gsap.to(leftEar.rotation, {x: Math.PI * .8, z: Math.PI * .1, ease: 'power2.inOut', yoyo: true, repeat: -1 , repeatDelay: 1})
+gsap.to(leftEar.position, {y: .2 ,ease: 'power2.inOut', yoyo: true, repeat: -1 , repeatDelay: 1 })
+
+gsap.to(rightEar.rotation, {x: Math.PI * .8, z: Math.PI * -.1, ease: 'power2.inOut', yoyo: true, repeat: -1 , repeatDelay: .5})
+gsap.to(rightEar.position, {y: .2 ,ease: 'power2.inOut', yoyo: true, repeat: -1 , repeatDelay: .5 })
+
+// Body Animations
+
+gsap.to(body.rotation , {x: Math.PI * -.85, ease: 'sine.inOut', yoyo: true, repeat: -1 , repeatDelay: 1})
+
+// Hand Animations
+
+gsap.to(rightHand.rotation, {z: Math.PI * -.85, ease: 'power4.inOut', yoyo: true, repeat: -1, repeatDelay: .2})
+gsap.to(leftHand.rotation, {z: Math.PI * .85, ease: 'power4.inOut', yoyo: true, repeat: -1, repeatDelay: .2})
+// Whole Body Animation
+
+gsap.to(allay.position, {y: .05 , ease: 'sine.inOut', yoyo: true, repeat: -1 , repeatDelay: .2 })
+
+scene.add(allay)
 const clock = new THREE.Clock()
 const tick = () =>{
-    const elapsedTime = clock.getElapsedTime()
-
     controls.update()
     renderer.render( scene , camera );
     window.requestAnimationFrame(tick)
 }
-
 tick()
